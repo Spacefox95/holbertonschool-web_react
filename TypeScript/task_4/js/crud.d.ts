24e0e98fd94c6bec7afda94d3c09f10bac83d28c
Spacefox95 <nathan.raynal@gmail.com>
2025-07-11 09:46:00 +0200

    ok

--- TypeScript/task_4/js/crud.d.ts
+++
@@ -1,8 +0,0 @@
-import { deleteRow, insertRow, updateRow } from "./crud";
-import { RowId, RowElement } from "interface";
-
-export function insertRow(row: RowElement): RowId;
-
-export function deleteRow(rowId: RowId): void;
-
-export function updateRow(rowId: RowId, row: RowElement): RowId;
---
+++ TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export class Subject {
+    public teacher!: Teacher;
+
+    set setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}

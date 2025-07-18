8ab9f329a7eebfd9dd617c3c05614a17be38f19a
Spacefox95 <nathan.raynal@gmail.com>
2025-07-18 11:46:08 +0200

    ok

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+import { debounce } from "lodash";
+import '../css/main.css';
+
+
+let count = 0;
+
+$('body').prepend('<div id="logo"></div>')
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+	count++;
+	$('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));

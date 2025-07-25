a07197cef103adea0972dcbaa276dbd936c68a1a
Spacefox95 <nathan.raynal@gmail.com>
2025-07-25 15:28:01 +0200

    ok

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import Notifications from './Notifications.jsx'
 import logo from './assets/holberton-logo.jpg';
-import { getCurrentYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <>
-      <div className="root-notifications">
-        <Notifications /></div>
       <div className="App-header">
         <img src={logo} alt="holberton logo" />
         <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
@@ -18,8 +14,7 @@ function App() {
       </div>
 
       <div className="App-footer">
-
-        <p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
+        <p>Copyright 2025 - holberton School</p>
       </div>
     </>
   );

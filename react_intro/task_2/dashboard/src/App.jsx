b0ab2a59497ec466eff24ee3300f3fd3e54bdfb4
Spacefox95 <nathan.raynal@gmail.com>
2025-07-28 09:54:16 +0200

    ok

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -18,10 +18,10 @@ function App() {
         <p>Login to access the full dashboard</p>
 
         <form>
-          <label for="email">Email</label>
+          <label for="email">Email: </label>
           <input type="email" id="email" name="email" />
 
-          <label for="password">Password</label>
+          <label for="password">Password: </label>
           <input type="password" id="password" name="password" />
 
           <button type="submit">OK</button>

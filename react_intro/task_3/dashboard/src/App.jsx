a3885e2e7c519368930eeb9da28dfa5089bec926
Spacefox95 <nathan.raynal@gmail.com>
2025-07-28 10:25:01 +0200

    ok

---
+++ react_intro/task_3/dashboard/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import Notifications from "./Notifications.jsx";
+import logo from "./assets/holberton-logo.jpg";
+import { getCurrentYear, getFooterCopy } from "./utils";
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        <form>
+          <label for="email">Email: </label>
+          <input type="email" id="email" name="email" />
+
+          <label for="password">Password: </label>
+          <input type="password" id="password" name="password" />
+
+          <button type="submit">OK</button>
+        </form>
+      </div>
+
+      <div className="App-footer">
+        <p>
+          Copyright {getCurrentYear()} - {getFooterCopy(false)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;

6d1a93211f1cd031135f8a672d19c9eb2ef18df0
Spacefox95 <nathan.raynal@gmail.com>
2025-07-29 15:32:46 +0200

    ok

--- react_intro/task_4/dashboard/vite.config.js
+++ react_intro/task_4/dashboard/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/holbertonschool-web_react/",
+  base: "./",
   plugins: [react()],
-})
+  webServer: {
+    command: "npm run dev -- --host 0.0.0.0",
+    url: "http://localhost:5173",
+    reuseExistingServer: false,
+    timeout: 120000,
+  },
+});

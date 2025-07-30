d63a2028363c72dd0b404c02478fa299ad80f160
Spacefox95 <nathan.raynal@gmail.com>
2025-07-30 14:14:19 +0200

    ok

--- react_props/task_0/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_0/dashboard/src/Footer/Footer.spec.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Footer from './Footer';
-import * as utils from '../../utils';
 
-describe('Footer component', () => {
-  test('renders correct copyright text', () => {
-    jest.spyOn(utils, 'getFooterCopy').mockReturnValue('Holberton School');
-    jest.spyOn(utils, 'getCurrentYear').mockReturnValue(2025);
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Footer from "./Footer";
+import * as utils from "../utils/utils";
+
+describe("Footer component", () => {
+  test("renders correct copyright text", () => {
+    jest.spyOn(utils, "getFooterCopy").mockReturnValue("Holberton School");
+    jest.spyOn(utils, "getCurrentYear").mockReturnValue(2025);
 
     render(<Footer />);
-    expect(screen.getByText(/©? ?2025 - holberton school/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/2025 - holberton school/i)
+    ).toBeInTheDocument();
 
     jest.restoreAllMocks();
   });

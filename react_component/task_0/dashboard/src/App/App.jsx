c9f5764c388f092f8878a67d9722e74a14ff646d
Spacefox95 <nathan.raynal@gmail.com>
2025-08-04 09:14:22 +0200

    ok

---
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import Header from "../Header/Header.jsx";
+import Footer from "../Footer/Footer.jsx";
+import Login from "../Login/Login.jsx";
+import Notifications from "../Notifications/Notifications.jsx";
+import { Component, Fragment } from "react";
+import CourseList from "../CourseList/CourseList.jsx";
+
+class App extends Component {
+  static defaultProps = {
+    isLoggedIn: false,
+  };
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const notificationsList = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "urgent", html: { __html: "<strong>Urgent requirement</strong> - complete by EOD" } },
+    ];
+
+    const courseList = []
+
+    return (
+      <Fragment>
+        <Notifications notifications={notificationsList} />
+        <Header />
+        {isLoggedIn ? (
+          <CourseList courses={courseList} />
+        ) : (
+          <Login />
+        )}
+        <Footer />
+      </Fragment>
+    );
+  }
+}
+
+export default App;

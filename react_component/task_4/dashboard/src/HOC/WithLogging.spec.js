461d06f0144418b2dd95f62da11a503f2c2cf419
Spacefox95 <nathan.raynal@gmail.com>
2025-08-04 14:44:20 +0200

    ok

---
+++ react_component/task_4/dashboard/src/HOC/WithLogging.spec.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import WithLogging from "./WithLogging";
+import { Component } from "react";
+
+class MockApp extends Component {
+  render() {
+    return <h1>Hello from Mock App Component</h1>;
+  }
+}
+
+describe("WithLogging HOC", () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleSpy.mockRestore();
+    cleanup();
+  });
+
+  test("renders heading from wrapped component", () => {
+    const MockAppLog = WithLogging(MockApp);
+    render(<MockAppLog />);
+    expect(
+      screen.getByText(/Hello from Mock App Component/i)
+    ).toBeInTheDocument();
+  });
+
+  test("logs mounts and unmount messages", () => {
+    const MockAppLog = WithLogging(MockApp);
+    const { unmount } = render(<MockAppLog />);
+    expect(consoleSpy).toHaveBeenCalledWith("Component MockApp is mounted");
+    unmount();
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Component MockApp is going to unmount"
+    );
+  });
+});

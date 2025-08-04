598b1042ee77e6020b95f4f7837669bafa65143e
Spacefox95 <nathan.raynal@gmail.com>
2025-08-04 15:38:47 +0200

    ok

--- react_component/task_4/dashboard/src/Notifications/NotificationItem.jsx
+++ react_component/task_4/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,23 +1,50 @@
-import { Component, createRef } from 'react';
+import React, { Component, createRef } from 'react';
 
 class NotificationItem extends Component {
     constructor(props) {
         super(props);
         this.liRef = createRef();
-    };
-
+    }
 
     componentDidMount() {
-        const { type } = this.props;
-        const color = type === "urgent" ? "red" : "blue";
+        this.updateColor();
+    }
+
+    componentDidUpdate() {
+        this.updateColor();
+    }
+
+    updateColor = () => {
+        const { type = 'default' } = this.props;
+        const colors = {
+            urgent: 'red',
+            default: 'blue'
+        };
+
+        const color = colors[type];
+
         if (this.liRef.current) {
             this.liRef.current.style.color = color;
-            this.liRef.current.style._values = { color };
+            if (!this.liRef.current.style._values) {
+                this.liRef.current.style._values = {};
+            }
+            this.liRef.current.style._values.color = color;
         }
     }
 
+    handleClick = () => {
+        const { id, markAsRead } = this.props;
+        if (markAsRead) {
+            markAsRead(id);
+        }
+    }
+
+    containsHTML = (str) => {
+        return typeof str === 'string' && /<\/?[a-z][\s\S]*>/i.test(str);
+    };
+
     render() {
-        const { type = "default", html, value } = this.props
+        const { type = 'default', html, value } = this.props;
 
         if (html) {
             return (
@@ -25,6 +52,17 @@ class NotificationItem extends Component {
                     ref={this.liRef}
                     data-notification-type={type}
                     dangerouslySetInnerHTML={html}
+                    onClick={this.handleClick}
+                />
+            );
+        }
+
+        if (value && this.containsHTML(value)) {
+            return (
+                <li
+                    ref={this.liRef}
+                    data-notification-type={type}
+                    dangerouslySetInnerHTML={{ __html: value }}
                     onClick={this.handleClick}
                 />
             );

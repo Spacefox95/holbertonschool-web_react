f4ec6b3c423e47af2bbf1d93bec1159d181745cb
Spacefox95 <nathan.raynal@gmail.com>
2025-08-05 09:14:38 +0200

    ok

--- react_component/task_5/dashboard/src/Notifications/Notifications.jsx
+++ react_component/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -8,6 +8,8 @@ class Notifications extends Component {
         console.log(`Notification ${id} has been marked as read`);
     }
 
+    shouldComponentUpdate(nextProps) { return nextProps.notifications.length !== this.props.notifications.length; }
+
     render() {
         const { notifications = [], displayDrawer = false } = this.props;
         let drawerContent = null;

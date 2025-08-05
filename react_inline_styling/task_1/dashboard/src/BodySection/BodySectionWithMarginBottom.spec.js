7136454f313e7b8c46d44ee0f0d11426abaafe0f
Spacefox95 <nathan.raynal@gmail.com>
2025-08-05 11:27:55 +0200

    ok

--- react_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.spec.js
+++ react_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.spec.js
@@ -2,16 +2,6 @@ import { render } from "@testing-library/react";
 import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
 
 describe("BodySectionWithMarginBottom", () => {
-  test("contains a div with the class bodySectionWithMargin", () => {
-    const { container } = render(
-
-      <BodySectionWithMarginBottom title="Test">
-        <p>Content</p>
-      </BodySectionWithMarginBottom>
-    );
-    expect(container.firstChild).toHaveClass("bodySectionWithMargin");
-  });
-
   test("renders the BOdySection component with correct content", () => {
     const { getByText } = render(
       <BodySectionWithMarginBottom title="Test title">

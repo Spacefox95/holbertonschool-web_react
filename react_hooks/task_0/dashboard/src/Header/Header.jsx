8b5e05010b8656bf3e2b262807caf136f7f75587
Spacefox95 <nathan.raynal@gmail.com>
2025-09-12 10:39:46 +0200

    ok

--- react_hooks/task_0/dashboard/src/Header/Header.jsx
+++ react_hooks/task_0/dashboard/src/Header/Header.jsx
@@ -4,45 +4,51 @@ import { StyleSheet, css } from "aphrodite";
 import { newContext } from "./../Context/context";
 
 const Header = () => {
-  const { user, logOut } = useContext(newContext);
+    const { user, logOut } = useContext(newContext);
 
-  return (
-    <div className={css(styles.header)}>
-      <img
-        className={css(styles.logo)}
-        src={holbertonLogo}
-        alt="holberton logo"
-      />
-      <h1 className={css(styles.title)}>School Dashboard</h1>
+    return (
+        <div className={css(styles.header)}>
+            <img
+                className={css(styles.logo)}
+                src={holbertonLogo}
+                alt="holberton logo"
+            />
+      // Before
+            <h1 className={css(styles.h1)}>School Dashboard</h1>
 
-      {user.isLoggedIn && (
-        <section id="logoutSection" className={css(styles.logOutSection)}>
-          <p>
-            Welcome{user.email}
-            <a href="#logout" onClick={logOut()}>
-              (logout)
-            </a>
-          </p>
-        </section>
-      )}
-    </div>
-  );
+// After
+            <h1 className={css(styles.h1)} style={{ color: "#e1003c" }}>
+                School Dashboard
+            </h1>
+
+            {user.isLoggedIn && (
+                <section id="logoutSection" className={css(styles.logOutSection)}>
+                    <p>
+                        Welcome{user.email}
+                        <a href="#logout" onClick={logOut()}>
+                            (logout)
+                        </a>
+                    </p>
+                </section>
+            )}
+        </div>
+    );
 };
 const styles = StyleSheet.create({
-  header: {
-    display: "flex",
-    fontFamily: `'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif`,
-    borderBottom: "solid #e1003c 1px",
-  },
-  logo: {
-    width: "150px",
-    height: "150px",
-  },
-  title: {
-    display: "flex",
-    alignItems: "center",
-    color: "#e1003c",
-  },
+    header: {
+        display: "flex",
+        fontFamily: `'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif`,
+        borderBottom: "solid #e1003c 1px",
+    },
+    logo: {
+        width: "150px",
+        height: "150px",
+    },
+    title: {
+        display: "flex",
+        alignItems: "center",
+        color: "#e1003c",
+    },
 });
 
 export default Header;

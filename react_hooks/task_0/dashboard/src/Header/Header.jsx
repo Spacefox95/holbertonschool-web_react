8e1a04cce4cf3a364639eb0645ea09ad31c32cae
Spacefox95 <nathan.raynal@gmail.com>
2025-09-12 09:57:11 +0200

    ok

--- react_hooks/task_0/dashboard/src/Header/Header.jsx
+++ react_hooks/task_0/dashboard/src/Header/Header.jsx
@@ -1,55 +1,48 @@
 import { useContext } from "react";
 import holbertonLogo from "../assets/holberton-logo.jpg";
 import { StyleSheet, css } from "aphrodite";
-import { newContext } from './../Context/context'
+import { newContext } from "./../Context/context";
 
 const Header = () => {
-    const { user, logOut } = useContext(newContext)
+  const { user, logOut } = useContext(newContext);
 
-    return (
-        <div className={css(styles.header)}>
-            <img
-                className={css(styles.logo)}
-                src={holbertonLogo}
-                alt="holberton logo"
-            />
-            <h1 className={css(styles.title)}>school Dashboard</h1>
+  return (
+    <div className={css(styles.header)}>
+      <img
+        className={css(styles.logo)}
+        src={holbertonLogo}
+        alt="holberton logo"
+      />
+      <h1 className={css(styles.title)}>School Dashboard</h1>
 
-            {user && user.isLoggedIn && (
-                <section id="logoutSection" className={css(styles.logOutSection)}>
-                    <p>
-                        Welcome <b>{user.email}</b>&nbsp;
-                        <a
-                            href="#logout"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                logOut();
-                            }}
-                        >
-                            (logout)
-                        </a>
-                    </p>
-                </section>
-            )}
-        </div>
-
-    );
+      {user.isLoggedIn && (
+        <section id="logoutSection" className={css(styles.logOutSection)}>
+          <p>
+            Welcome{user.email}
+            <a href="#logout" onClick={logOut()}>
+              (logout)
+            </a>
+          </p>
+        </section>
+      )}
+    </div>
+  );
 };
 const styles = StyleSheet.create({
-    header: {
-        display: "flex",
-        fontFamily: `'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif`,
-        borderBottom: "solid #e1003c 1px",
-    },
-    logo: {
-        width: "150px",
-        height: "150px",
-    },
-    title: {
-        display: "flex",
-        alignItems: "center",
-        color: "#e1003c",
-    },
+  header: {
+    display: "flex",
+    fontFamily: `'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif`,
+    borderBottom: "solid #e1003c 1px",
+  },
+  logo: {
+    width: "150px",
+    height: "150px",
+  },
+  title: {
+    display: "flex",
+    alignItems: "center",
+    color: "#e1003c",
+  },
 });
 
 export default Header;

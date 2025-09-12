55728dfa5bf31768733ba6ef6b8ceaafa084924a
Spacefox95 <nathan.raynal@gmail.com>
2025-09-12 09:25:30 +0200

    ok

--- react_hooks/task_0/dashboard/src/Header/Header.spec.js
+++ react_hooks/task_0/dashboard/src/Header/Header.spec.js
@@ -12,26 +12,13 @@ afterEach(() => {
 });
 
 describe("Header component with context", () => {
-  test("logoutSection not rendered with default context", () => {
-    render(<Header />);
-    const logoutSection = screen.queryByText(/logout/i);
-    expect(logoutSection).not.toBeInTheDocument();
-  });
-
-  test("logoutSection rendered when user is logged in", () => {
-    const user = { email: "user@test.com", password: "12345678", isLoggedIn: true };
-    render(
-      <newContext.Provider value={{ user, logOut: jest.fn() }}>
-        <Header />
-      </newContext.Provider>
-    );
-    const logoutSection = screen.getByText(/welcome user@test.com/i);
-    expect(logoutSection).toBeInTheDocument();
-  });
-
   test("clicking logout calls logOut function", () => {
     const logOutMock = jest.fn();
-    const user = { email: "user@test.com", password: "12345678", isLoggedIn: true };
+    const user = {
+      email: "user@test.com",
+      password: "12345678",
+      isLoggedIn: true,
+    };
     render(
       <newContext.Provider value={{ user, logOut: logOutMock }}>
         <Header />

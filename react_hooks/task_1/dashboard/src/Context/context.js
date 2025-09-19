f4b03436bd5c5d7d4801f62c49963b345beca613
Spacefox95 <nathan.raynal@gmail.com>
2025-09-19 09:41:12 +0200

    ok

---
+++ react_hooks/task_1/dashboard/src/Context/context.js
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export const defaultUser = {
+  email: "",
+  password: "",
+  isLoggedIn: false,
+};
+
+export const defaultLogOut = () => {};
+
+export const newContext = createContext({
+  user: defaultUser,
+  logOut: defaultLogOut,
+});
